565acefc86fbbfdfe9d1c89f8f489a8ad9aed3fe
Yifan Yu <724325033@qq.com>
2017-02-18 15:20:28 -0800

    Create 2.64.c

---
+++ 2.64.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int any_odd_one(unsigned x){
+    //meet the requirement of inter coding rules
+    int y = 0xAAAAAAAA;//32bit all even bits =1
+    return (x & y)!=0;
+}
+
+int main(){
+	
+	int i = any_odd_one(0x55555555);
+	printf("is it any odd one? for 0x55555555 %i\n",i);
+    
+    int b = any_odd_one(0xFFFFFFFF);
+    printf("is it any odd one? for 0xFFFFFFFF %i\n",b);
+
+    int c = any_odd_one(0x03);
+    printf("is it any odd one? for 0x03  %i\n",c);
+	
+}
+
+
+
